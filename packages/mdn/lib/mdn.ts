e423e56b9a9ae5f900cb72101423b18814826f9d
ddiablou <jekem13936@jo6s.com>
2023-05-31 07:55:24 +0200

    fix(mdn): user agent

--- packages/mdn/lib/mdn.ts
+++ packages/mdn/lib/mdn.ts
@@ -1,8 +1,10 @@
 import { fetch, FetchResultTypes } from "@sapphire/fetch";
 import type { Root } from "./types";
+import os from "node:os";
 
 export class Client {
 	private readonly url: string = "https://developer.mozilla.org";
+	private readonly user_agent: string = `@zeyrbot/mdn/1.0.0 (node-fetch) ${os.platform()}/${os.release()} (https://github.com/zeyrbot/packages/tree/main/packages/mdn)`;
 
 	public async search(query: string, size = 20) {
 		const fullUrl = new URL(`${this.url}/api/v1/search`);
@@ -14,6 +16,7 @@ export class Client {
 			{
 				headers: {
 					"Content-Type": "application/json",
+					"User-Agent": this.user_agent,
 				},
 			},
 			FetchResultTypes.JSON,
